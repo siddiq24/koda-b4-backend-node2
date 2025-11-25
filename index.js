e3e5d4b6e900b49a833e958a385bbb129da55593
siddiq24 <siddiqrm24@gmail.com>
2025-11-25 13:06:16 +0700

    fix: fixing initialize route

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = require('./src/routers');
 
 app.use(express.json());
 app.use(express.urlencoded());
 
-const productRouter = require('./src/routers/product.router');
-app.use('/', productRouter);
-
-const authRouter = require('./src/routers/auth.router');
-app.use('/auth', authRouter);
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const productController = require('../controllers/products.controller');
-router.get('/products', productController.getAllProducts);
-router.get('/products/:id', productController.getProductsById);
-router.post('/products', productController.createNewProduct);
-router.patch('/products/:id', productController.updateProduct);
-router.delete('/products/:id', productController.deleteProduct);
+router.get('', productController.getAllProducts);
+router.get('/:id', productController.getProductsById);
+router.post('', productController.createNewProduct);
+router.patch('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
