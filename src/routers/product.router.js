ffceb854ace0c7cbd87b377065e1800a64d841b6
siddiq24 <siddiqrm24@gmail.com>
2025-11-26 18:08:30 +0700

    feat: enhance validator and implemnet middleware authentications

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const { body, param } = require('express-validator');
+const authenticate = require("../middlewares/auth.middleware");
 
 const productController = require('../controllers/products.controller');
 router.get('',
     param('search').optional().isString(),
     productController.getAllProducts);
-router.get('/:id', productController.getProductsById);
+
+router.get('/:id',
+    param('id').isInt().withMessage('Product ID must be an integer'),
+    productController.getProductsById);
+
 router.post('',
     body('name').notEmpty().withMessage('Name is required'),
     body('price').isNumeric().withMessage('Price must be a number'),
+    authenticate,
     productController.createNewProduct);
+
 router.patch('/:id',
-
+    body('name').optional().notEmpty().withMessage('Name cannot be empty'),
+    body('price').optional().isNumeric().withMessage('Price must be a number'),
+    body('categoryId').optional().isInt().withMessage('Category ID must be an integer'),
+    body('description').optional().isString().withMessage('Description must be a string'),
+    authenticate,
     productController.updateProduct);
+
 router.delete('/:id',
-
+    param('id').isInt().withMessage('Product ID must be an integer'),
+    authenticate,
     productController.deleteProduct);
-router.post('/:id/upload',
 
+router.post('/:id/upload',
+    param('id').isInt().withMessage('Product ID must be an integer'),
+    body('picture').notEmpty().withMessage('Picture is required'),
+    authenticate,
     productController.uploadPictureProduct);
 
 module.exports = router;
