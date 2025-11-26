a27e3e1b8bdb090a91c97e6f809970bc13b67332
siddiq24 <siddiqrm24@gmail.com>
2025-11-26 17:26:44 +0700

    fix: fixing parameter

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,18 @@
 const prisma = require('../libs/prisma');
 
 module.exports = {
-    register: async (userData) => {
+    register: async ({ email, password }) => {
         const newUser = await prisma.user.create({
             data: {
-                email: userData.email,
-                password: userData.password,
+                email: email,
+                password: password
             }
         });
-
         return newUser;
     },
 
     findUserByEmail: async (email) => {
-        return await prisma.user.findUnique({
+        return prisma.user.findUnique({
             where: { email }
         });
     }
